1fe22b376271724e052e2f5ededb195786db6196
Zhenzhou Li <z5691907@ad.unsw.edu.au>
2026-03-22 16:02:28 +1100

    file setups

---
+++ src/models.py
@@ -0,0 +1,5 @@
+# define our different ML models here
+# leo's note, im gonna be trying random forests first
+# other things could be interesting too
+
+# we should probably compare deep learning methods to more traditiona ML pipelines in our write up
---
+++ src/preprocessing.py
@@ -0,0 +1,13 @@
+# leo's note
+# median and bilateral filters are both good at preserving edges but smoothes out
+# regions of similar 'appearance'
+
+# an example of how we could use this is
+# image -> apply filter -> superpixel clustering -> label superpixels in model
+
+def apply_median_filter():
+    # 
+    pass
+
+def apply_bilateral_filter():
+    pass
